b3e240afd6c19a586cfe8264b3af2b02a6f89a03
sanket.nagone@gmail.com <sanket.nagone@gmail.com>
2017-03-08 23:47:19 +0530

    new commit

--- MyFirstApplication/MyFirstApplication/Program.cs
+++ MyFirstApplication/MyFirstApplication/Program.cs
@@ -10,6 +10,8 @@ namespace MyFirstApplication
             Console.WriteLine("Dev 1 Commit");
             Console.WriteLine("Dev 2 Commit");
             Console.WriteLine("Changes");
+            Console.WriteLine("Dev 2 Commit");
+
         }
     }
 }
